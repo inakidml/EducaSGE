5719815464a9d5ac7d9e89ba76dc048f9c962e8f
Iñaki <inaki.diaz@ikasle.egibide.org>
2017-05-24 19:59:53 +0200

    constraints y campo dni

--- educasge/models/models.py
+++ educasge/models/models.py
@@ -11,6 +11,7 @@ class educasge(models.Model):
     photo = fields.Binary("Image", help="Ponga aquí su imagen")
     nombre = fields.Char(required="true")
     apellido = fields.Char(required="true")
+    dni = fields.Char('DNI', required="true")
     # page informacion personal
     sexo = fields.Selection([('H', 'Hombre'), ('M', 'Mujer')], 'Sexo')
     nacionalidad = fields.Char()
@@ -23,10 +24,23 @@ class educasge(models.Model):
     alumnocursoids = fields.One2many('educasge.alumnoscursos', 'educasge_id', 'Notas cursos')
 
 
+    _sql_constraints = [
+        ('unique_dni',
+         'unique(dni)',
+         'El campo DNI, debe ser unico')
+        ]
+
+    @api.one
+    @api.constrains('fechaNacimiento')
+    def _check_birthdate(self):
+        if self.fechaNacimiento > fields.Date.today():
+            raise ValidationError(("La fecha de nacimiento no puede ser posterior a la actual!"))
+
+
 class cursos(models.Model):
     _name = 'educasge.cursos'
 
-    nombre = fields.Char(string='Nombre', size=32, required=True)
+    name = fields.Char(string='Nombre', size=32, required=True)
     horas = fields.Char(string='Horas', size=3, required=True)
     descripcion = fields.Char(string='Decripcion', size=500)
 
@@ -36,7 +50,6 @@ class educasgealumnoscursos(models.Model):
 
     educasge_id = fields.Many2one('educasge.educasge', 'Alumno', ondelete="cascade")
     cursos_id = fields.Many2one('educasge.cursos', 'Curso')
-    nombre = fields.Many2one('educasge.cursos', 'Nombre')
     nota = fields.Char();
 
     #     value = fields.Integer()
@@ -47,9 +60,9 @@ class educasgealumnoscursos(models.Model):
     #     def _value_pc(self):
     #        self.value2 = float(self.value) / 100
 
+    _sql_constraints = [
+        ('unique_curso_alummo',
+         'UNIQUE(educasge_id, cursos_id)',
+         'El alumno solo puede estar inscrito una vez a cada curso')
+    ]
 
-    @api.one
-    @api.constrains('fechaNacimiento')
-    def _check_birthdate(self):
-        if self.fechaNacimiento > fields.Date.today():
-            raise ValidationError(_("Birth Date can't be greater than current date!"))
